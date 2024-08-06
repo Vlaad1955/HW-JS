65f1e4dcb8a219bd6c3dcb706d0afab9bc2fc0fa
Vlaad1955 <wylf1312@gmail.com>
2024-08-06 12:47:53 +0300

    add HW1

---
+++ HW1/HW1.js
@@ -0,0 +1,35 @@
+let A1 = "hello";
+let A2 = "owu";
+let A3 = "com"
+let A4 = "ua";
+let A5 = 1;
+let A6=10;
+let A7= -999;
+let A8=123;
+let A9 = 3.14;
+let A10= 2.7;
+let A11 =16;
+let A12 = true;
+let A13 = false;
+
+console.log(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13);
+
+let firstName = "Vladyslav";
+let middleName = "Volodymyrovych";
+let lastName= "Rybalko";
+
+console.log(lastName+" "+firstName+" "+middleName);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+let PromtFirstName = prompt("Введіть ваше ім'я:");
+let PromtMiddleName = prompt("Введіть ваше по-батькові:");
+let PromtAge = prompt("Введіть ваш вік:");
+
+console.log(`Ваше ім'я - ${PromtFirstName} ${PromtMiddleName} ваш вік - ${PromtAge}`);
